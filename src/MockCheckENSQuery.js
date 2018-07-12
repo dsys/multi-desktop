ce7ee00cee7dfe8307cd867e1d4d7ee2e9e3f62c
Jackson Callaway <jcksncllwy@gmail.com>
2018-07-11 18:17:31 -0700

    more work on the username registration

--- src/MockCheckENSQuery.js
+++ src/MockCheckENSQuery.js
@@ -5,16 +5,21 @@ import MockQuery from "./MockQuery";
 const GQL_query_checkENS = gql(`
   query checkENS($subdomain: String!) {
     checkENS(subdomain: $subdomain){
-      owner
+      available
     }
   }
 `);
 
 export default class MockCheckENSQuery extends React.Component {
   render(){
-    const {loading=false, error=null, skip, mockData, subdomain} = this.props;
+    const {loading=false, error=null, skip, subdomain} = this.props;
+    const data = {
+      checkENS: {
+        available: subdomain!="skrillex"
+      }
+    }
     return(
-      <MockQuery skip={skip} loading={loading} error={error} mockData={mockData} query={GQL_query_checkENS} variables={{subdomain}}>
+      <MockQuery skip={skip} loading={loading} error={error} mockData={data} query={GQL_query_checkENS} variables={{subdomain}}>
         {this.props.children}
       </MockQuery>
     )
--- src/RegisterScreen.js
+++ src/RegisterScreen.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { gql } from "apollo-boost";
 import { Query } from "react-apollo";
 import MockCheckENSQuery from './MockCheckENSQuery'
+import { Link } from "react-router-dom";
 
 import { default as colors } from './colors';
 import ScreenStyles from "./ScreenStyles";
@@ -33,22 +34,88 @@ export default class RegisterScreen extends React.Component {
     const {subdomain} = this.state;
     return(
       <div className="screen-container">
-        <div className="header"></div>
-        <input value={subdomain} onChange={this.handleInput} name="subdomain" type="text" />
-        <MockCheckENSQuery skip={!subdomain} subdomain={subdomain} mockData={"YOYOYO"}>
-          {({ loading, error, data }) => {
-            if (loading) return `Loading...`;
-            if (error) return `Error!: ${error}`;
-            return (
-              <div className="ens-check-results">
-                {JSON.stringify(data, null, 4)}
-              </div>
-            );
-          }}
-        </MockCheckENSQuery>
+        <div className="shrinkwrap-container">
+          <div className="header">Pick A Username</div>
+          <input className="subdomain-input" value={subdomain} onChange={this.handleInput} name="subdomain" type="text" />
+          <div className="availability">
+            <MockCheckENSQuery skip={!subdomain} subdomain={subdomain}>
+              {({ loading, error, data }) => {
+                if (loading) return `Loading...`;
+                if (error) return `Error!: ${error}`;
+                return (
+                  <div className="ens-check-results">
+                    {data.checkENS.available?"available":"not available"}
+                  </div>
+                );
+              }}
+            </MockCheckENSQuery>
+          </div>
+          <div className="buttons">
+            <Link className="button" to="/welcome">
+              {`< Nevermind`}
+            </Link>
+            <Link className="button" to="/phone-verification">
+              {`Register >`}
+            </Link>
+          </div>
+        </div>
         {ScreenStyles}
         <style jsx>{`
+          .header {
+            font-family: Lobster;
+            font-size: 64px;
+            font-weight: 700;
+            text-shadow: 6px 6px 0px rgba(0, 0, 0, 0.2);
+            color: ${colors.white2};
+          }
 
+          .shrinkwrap-container{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+          }
+
+          .subdomain-input{
+            width: 100%;
+            box-sizing: border-box;
+            padding: 10px;
+            border: none;
+            border-radius: 5px;
+            margin-top: 10px;
+            font-size: 24px;
+            text-align: center;
+            color: ${colors.blue2};
+          }
+
+          .availability{
+            margin-top: 5px;
+            height: 1em;
+            color: ${colors.white2};
+          }
+
+          .buttons{
+            width: 100%;
+            margin-top: 20px;
+            display: flex;
+            flex-direction: row;
+            width: 100%;
+          }
+
+          .buttons :global(.button){
+            color: ${colors.blue2};
+            text-decoration: none;
+            background: ${colors.white2};
+            padding: 10px;
+            border-radius: 5px;
+            margin-left: 20px;
+            flex-grow: 1;
+            text-align: center;
+          }
+
+          .buttons :global(.button:first-child){
+            margin-left: 0;
+          }
         `}</style>
       </div>
     )
